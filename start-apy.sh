ecca1a9d562bf65fceaffeae79af2b91d5931971
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-26 16:02:03 +0800

    update

--- start-apy.sh
+++ start-apy.sh
@@ -2,6 +2,7 @@
 PID=$(ps -ef|grep apys.js|grep -v grep|awk '{printf $2}')
 if [ -z "$PID" ]
 then
+    cd /home/ubuntu/xswap/apyrobot
     nodejs apys.js
     cp apy.json /home/ubuntu/www/pool.json
     echo apy end
