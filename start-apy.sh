6130d94dca0f360931f0c7bcbe23bc1bb06f4562
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-26 15:10:08 +0800

    update

--- start-apy.sh
+++ start-apy.sh
@@ -5,9 +5,10 @@ then
     nodejs apys.js
     cp apy.json /home/ubuntu/www/pool.json
     echo apy end
+    sleep 1
     PID=$(ps -ef|grep apy.js|grep -v grep|awk '{printf $2}')
     if [ -z "$PID" ] then
-      kill -9 $PID
+        kill -9 $PID
     fi
 else
     echo apys is already runing
