f31c6dc38d795c62f085cef37c148ce8cbb226c5
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-26 15:11:54 +0800

    update

--- start-apy.sh
+++ start-apy.sh
@@ -7,7 +7,8 @@ then
     echo apy end
     sleep 1
     PID=$(ps -ef|grep apy.js|grep -v grep|awk '{printf $2}')
-    if [ -z "$PID" ] then
+    if [ -z "$PID" ]
+    then
         kill -9 $PID
     fi
 else
