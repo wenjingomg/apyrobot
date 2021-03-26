828327dda694e4acc0b828b02b41269ce8024b29
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-26 14:48:51 +0800

    update

--- start-apy.sh
+++ start-apy.sh
@@ -4,6 +4,9 @@ if [ -z "$PID" ]
 then
     nodejs apys.js
     echo apy end
+    PID=$(ps -ef|grep apy.js|grep -v grep|awk '{printf $2}')
+    if [ -z "$PID" ]
+    kill -9 $PID
 else
     echo apys is already runing
 fi
