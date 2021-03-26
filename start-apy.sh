b641069cafd67f2a6dea231a296dad8d8d80ec58
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-26 15:29:34 +0800

    update

--- start-apy.sh
+++ start-apy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-PID=$(ps -ef|grep apy.js|grep -v grep|awk '{printf $2}')
+PID=$(ps -ef|grep apys.js|grep -v grep|awk '{printf $2}')
 if [ -z "$PID" ]
 then
     nodejs apys.js
     cp apy.json /home/ubuntu/www/pool.json
     echo apy end
     sleep 1
-    PID=$(ps -ef|grep apy.js|grep -v grep|awk '{printf $2}')
+    PID=$(ps -ef|grep apys.js|grep -v grep|awk '{printf $2}')
     if [ -z "$PID" ]
     then
         kill -9 $PID
