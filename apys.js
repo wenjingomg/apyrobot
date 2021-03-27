679dd404361c2115a1774659a54137edf8b2af3a
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-27 17:13:07 +0800

    update

--- apys.js
+++ apys.js
@@ -114,7 +114,71 @@ const coins = [
     tokenAddresses: '0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281', 
     symbol: 'USDT',
     islp: false
+  },
+  {
+    pid: 16,
+    lpAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
+    tokenAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
+    symbol: 'OKB',
+    islp: false
+  },
+  {
+    pid: 17,
+    lpAddresses: '0x533367b864D9b9AA59D0DCB6554DF0C89feEF1fF', 
+    tokenAddresses: '0x533367b864D9b9AA59D0DCB6554DF0C89feEF1fF', 
+    symbol: 'USDK',
+    islp: false
+  },
+  {
+    pid: 18,
+    lpAddresses: '0x3e33590013B24bf21D4cCca3a965eA10e570D5B2', 
+    tokenAddresses: '0x3e33590013B24bf21D4cCca3a965eA10e570D5B2', 
+    symbol: 'USDC',
+    islp: false
+  },
+  {
+    pid: 19,
+    lpAddresses: '0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0', 
+    tokenAddresses: '0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0', 
+    symbol: 'BTCK',
+    islp: false
+  },
+  {
+    pid: 20,
+    lpAddresses: '0xDF950cEcF33E64176ada5dD733E170a56d11478E', 
+    tokenAddresses: '0xDF950cEcF33E64176ada5dD733E170a56d11478E', 
+    symbol: 'ETHK',
+    islp: false
+  },
+  {
+    pid: 21,
+    lpAddresses: '0x72f8fa5da80dc6e20e00d02724cf05ebd302c35f', 
+    tokenAddresses: '0x72f8fa5da80dc6e20e00d02724cf05ebd302c35f', 
+    symbol: 'DOTK',
+    islp: false
+  },
+  {
+    pid: 22,
+    lpAddresses: '0xf6a0Dc1fD1d2c0122ab075d7ef93aD79F02CcB93', 
+    tokenAddresses: '0xf6a0Dc1fD1d2c0122ab075d7ef93aD79F02CcB93', 
+    symbol: 'FILK',
+    islp: false
+  },
+  {
+    pid: 23,
+    lpAddresses: '0xd616388f6533B6f1c31968a305FbEE1727F55850', 
+    tokenAddresses: '0xd616388f6533B6f1c31968a305FbEE1727F55850', 
+    symbol: 'LTCK',
+    islp: false
+  },
+  {
+    pid: 25,
+    lpAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
+    tokenAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
+    symbol: 'WOKT',
+    islp: false
   }
+  
 ]
 
 //address
