57b8eade168d8f3f2ac90694a3f8779e542d994f
Wen Jing <wenjing@gomefinance.com.cn>
2021-04-07 08:58:23 +0800

    add gencoin, update new pool addr

--- apys.js
+++ apys.js
@@ -1,192 +1,154 @@
 // const hecoAddress = 'http://127.0.0.1:26659'
-const hecoAddress = 'https://exchaintest.okexcn.com'
+// const hecoAddress = 'https://exchaintest.okexcn.com'
+const hecoAddress = 'http://52.73.128.127:3100'
 
 // 类型, 池子， 锁仓/交易额, 日化
-const coins = [
-  {
-    pid: 0,
-    lpAddresses: '0x172a378B1aB64333bE2faa54C96476E5389F30AB', // 质押币地址 lpAddresses 统一用来查询余额，lp的话填写lp地址，单币的填币种地址
-    tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', // tokenAddresses,单币的话不需要，lp填要获取价格的币种地址
-    symbol: 'XT/USDT',
-    islp: true
-  },
-  {
-    pid: 1,
-    lpAddresses: '0x7B9A93174fC97d8CC42A632676C270FB25b80510', 
-    tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', 
-    symbol: 'XT/BTCK',
-    islp: true
-  },
-  {
-    pid: 2,
-    lpAddresses: '0x95c89c8F0506C1410F310b09B37D9cC1b5417BF9', 
-    tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', 
-    symbol: 'XT/OKB',
-    islp: true
-  },
-  {
-    pid: 3,
-    lpAddresses: '0xBd99824eB6DaaeC752151B75D1FD451F17A07434', 
-    tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', 
-    symbol: 'XT/ETHK',
-    islp: true
-  },
-  {
-    pid: 4,
-    lpAddresses: '0x554b9D5F5895a06E626527298A1634F82B330395', 
-    tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', 
-    symbol: 'XT/OKT',
-    islp: true
-  },
-  {
-    pid: 5,
-    lpAddresses: '0x48048fd8Fb1dD3Eb62BFB62cC21da359817AfaCD', 
-    tokenAddresses: '0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0', 
-    symbol: 'BTCK/USDT',
-    islp: true
-  },
-  {
-    pid: 6,
-    lpAddresses: '0x41a7bf2CB1fe7FE6e718acc31C387a24f354C92F', 
-    tokenAddresses: '0xDF950cEcF33E64176ada5dD733E170a56d11478E', 
-    symbol: 'ETHK/USDT',
-    islp: true
-  },
-  {
-    pid: 7,
-    lpAddresses: '0x6A8ed8f4aF0E80a4787018F7f20F8b90A4b62DB8', 
-    tokenAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
-    symbol: 'OKB/USDT',
-    islp: true
-  },
-  {
-    pid: 8,
-    lpAddresses: '0xF23E58F225E8Fb53fD8a564EC1A4D5D067025424', 
-    tokenAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
-    symbol: 'OKB/ETHK',
-    islp: true
-  },
-  {
-    pid: 9,
-    lpAddresses: '0xFd772dc34f78CAe86F4b66E3531249a794e102be', 
-    tokenAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
-    symbol: 'OKB/BTCK',
-    islp: true
-  },
-  {
-    pid: 10,
-    lpAddresses: '0x41439203945ef755CC53E0719e1C4eC0b01CF7F3', 
-    tokenAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
-    symbol: 'OKB/OKT',
-    islp: true
-  },
-  {
-    pid: 11,
-    lpAddresses: '0x2763233Cd2CA4A886b75047fE05663b45C985eb7', 
-    tokenAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
-    symbol: 'OKT/USDT ',
-    islp: true
-  },
-  {
-    pid: 12,
-    lpAddresses: '0x82F93401F603AFf80DF19A3EAd5D500dDA362213', 
-    tokenAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
-    symbol: 'OKT/ETHK',
-    islp: true
-  },
-  {
-    pid: 13,
-    lpAddresses: '0x5562dAF6F0bd0ed651Df008416D6C114E83e8221', 
-    tokenAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
-    symbol: 'OKT/BTCK',
-    islp: true
-  },
-  {
-    pid: 14,
-    lpAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', 
-    tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', 
-    symbol: 'XT',
-    islp: false
-  },
-  {
-    pid: 15,
-    lpAddresses: '0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281', 
-    tokenAddresses: '0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281', 
-    symbol: 'USDT',
-    islp: false
-  },
-  {
-    pid: 16,
-    lpAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
-    tokenAddresses: '0xDa9d14072Ef2262c64240Da3A93fea2279253611', 
-    symbol: 'OKB',
-    islp: false
-  },
-  {
-    pid: 17,
-    lpAddresses: '0x533367b864D9b9AA59D0DCB6554DF0C89feEF1fF', 
-    tokenAddresses: '0x533367b864D9b9AA59D0DCB6554DF0C89feEF1fF', 
-    symbol: 'USDK',
-    islp: false
-  },
-  {
-    pid: 18,
-    lpAddresses: '0x3e33590013B24bf21D4cCca3a965eA10e570D5B2', 
-    tokenAddresses: '0x3e33590013B24bf21D4cCca3a965eA10e570D5B2', 
-    symbol: 'USDC',
-    islp: false
-  },
-  {
-    pid: 19,
-    lpAddresses: '0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0', 
-    tokenAddresses: '0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0', 
-    symbol: 'BTCK',
-    islp: false
-  },
-  {
-    pid: 20,
-    lpAddresses: '0xDF950cEcF33E64176ada5dD733E170a56d11478E', 
-    tokenAddresses: '0xDF950cEcF33E64176ada5dD733E170a56d11478E', 
-    symbol: 'ETHK',
-    islp: false
-  },
-  {
-    pid: 21,
-    lpAddresses: '0x72f8fa5da80dc6e20e00d02724cf05ebd302c35f', 
-    tokenAddresses: '0x72f8fa5da80dc6e20e00d02724cf05ebd302c35f', 
-    symbol: 'DOTK',
-    islp: false
-  },
-  {
-    pid: 22,
-    lpAddresses: '0xf6a0Dc1fD1d2c0122ab075d7ef93aD79F02CcB93', 
-    tokenAddresses: '0xf6a0Dc1fD1d2c0122ab075d7ef93aD79F02CcB93', 
-    symbol: 'FILK',
-    islp: false
-  },
-  {
-    pid: 23,
-    lpAddresses: '0xd616388f6533B6f1c31968a305FbEE1727F55850', 
-    tokenAddresses: '0xd616388f6533B6f1c31968a305FbEE1727F55850', 
-    symbol: 'LTCK',
-    islp: false
-  },
-  {
-    pid: 24,
-    lpAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
-    tokenAddresses: '0x70c1c53E991F31981d592C2d865383AC0d212225', 
-    symbol: 'WOKT',
-    islp: false
-  }
-  
-]
+const coins = [{
+  "pid": 0,
+  "lpAddresses": "0xd1c0789E355D867fF133C88c77211A09aB833060",
+  "tokenAddresses": "0x2698598b98175bB5d20f69063d5CD99514D63819",
+  "symbol": "XT/USDT",
+  "isLp": true
+}, {
+  "pid": 1,
+  "lpAddresses": "0x13593B58738B31173a98402aC5077D4286dC9BF1",
+  "tokenAddresses": "0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0",
+  "symbol": "BTCK/XT",
+  "isLp": true
+}, {
+  "pid": 2,
+  "lpAddresses": "0xdb4AaA77E8b90e95bE2b038febDd02459222F856",
+  "tokenAddresses": "0x2698598b98175bB5d20f69063d5CD99514D63819",
+  "symbol": "XT/OKB",
+  "isLp": true
+}, {
+  "pid": 3,
+  "lpAddresses": "0x3CD27cBBADdCCD7e91C9F71955637eEbBd47B680",
+  "tokenAddresses": "0x2698598b98175bB5d20f69063d5CD99514D63819",
+  "symbol": "XT/ETHK",
+  "isLp": true
+}, {
+  "pid": 4,
+  "lpAddresses": "0x3EFa5b6620775cD9334a29736559f1e198E0E548",
+  "tokenAddresses": "0x2698598b98175bB5d20f69063d5CD99514D63819",
+  "symbol": "XT/WOKT",
+  "isLp": true
+}, {
+  "pid": 5,
+  "lpAddresses": "0x90994EB8aF4A82e2657ACd5e5CA86B378956D357",
+  "tokenAddresses": "0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0",
+  "symbol": "BTCK/USDT",
+  "isLp": true
+}, {
+  "pid": 6,
+  "lpAddresses": "0x31958B77EB55D23fe18823F15d070137CEbFF4F8",
+  "tokenAddresses": "0xDF950cEcF33E64176ada5dD733E170a56d11478E",
+  "symbol": "ETHK/USDT",
+  "isLp": true
+}, {
+  "pid": 7,
+  "lpAddresses": "0x81f4D6F37edce7737162b411617fb0Aea04Ec73E",
+  "tokenAddresses": "0xDa9d14072Ef2262c64240Da3A93fea2279253611",
+  "symbol": "OKB/USDT",
+  "isLp": true
+}, {
+  "pid": 8,
+  "lpAddresses": "0xDd22102698A574A28AFD80133715e8B81cCE3eC3",
+  "tokenAddresses": "0xDa9d14072Ef2262c64240Da3A93fea2279253611",
+  "symbol": "OKB/ETHK",
+  "isLp": true
+}, {
+  "pid": 9,
+  "lpAddresses": "0x9094493f729b68c1F18F471839920f3228c18399",
+  "tokenAddresses": "0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0",
+  "symbol": "BTCK/OKB",
+  "isLp": true
+}, {
+  "pid": 10,
+  "lpAddresses": "0xcb686638708DD442c8F8faf6fEEcdF5f6e4eb04C",
+  "tokenAddresses": "0x70c1c53E991F31981d592C2d865383AC0d212225",
+  "symbol": "WOKT/OKB",
+  "isLp": true
+}, {
+  "pid": 11,
+  "lpAddresses": "0x9db50c36ABc32faCc7b66a647E9183d9e503D614",
+  "tokenAddresses": "0x70c1c53E991F31981d592C2d865383AC0d212225",
+  "symbol": "WOKT/USDT",
+  "isLp": true
+}, {
+  "pid": 12,
+  "lpAddresses": "0xC73c9623fFA1DF6a0896d8A3E4aa240ECA15195a",
+  "tokenAddresses": "0x70c1c53E991F31981d592C2d865383AC0d212225",
+  "symbol": "WOKT/ETHK",
+  "isLp": true
+}, {
+  "pid": 13,
+  "lpAddresses": "0xE096CB4ea6b8c07bf562bf0CC810BcB94Dd6bceF",
+  "tokenAddresses": "0xE096CB4ea6b8c07bf562bf0CC810BcB94Dd6bceF",
+  "symbol": "OKXT",
+  "isLp": false
+}, {
+  "pid": 14,
+  "lpAddresses": "0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0",
+  "tokenAddresses": "0x09973e7e3914EB5BA69C7c025F30ab9446e3e4e0",
+  "symbol": "BTCK",
+  "isLp": false
+}, {
+  "pid": 15,
+  "lpAddresses": "0xDa9d14072Ef2262c64240Da3A93fea2279253611",
+  "tokenAddresses": "0xDa9d14072Ef2262c64240Da3A93fea2279253611",
+  "symbol": "OKB",
+  "isLp": false
+}, {
+  "pid": 16,
+  "lpAddresses": "0xDF950cEcF33E64176ada5dD733E170a56d11478E",
+  "tokenAddresses": "0xDF950cEcF33E64176ada5dD733E170a56d11478E",
+  "symbol": "ETHK",
+  "isLp": false
+}, {
+  "pid": 17,
+  "lpAddresses": "0x70c1c53E991F31981d592C2d865383AC0d212225",
+  "tokenAddresses": "0x70c1c53E991F31981d592C2d865383AC0d212225",
+  "symbol": "WOKT",
+  "isLp": false
+}, {
+  "pid": 18,
+  "lpAddresses": "0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281",
+  "tokenAddresses": "0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281",
+  "symbol": "USDT",
+  "isLp": false
+}, {
+  "pid": 19,
+  "lpAddresses": "0x3e33590013B24bf21D4cCca3a965eA10e570D5B2",
+  "tokenAddresses": "0x3e33590013B24bf21D4cCca3a965eA10e570D5B2",
+  "symbol": "USDC",
+  "isLp": false
+}, {
+  "pid": 20,
+  "lpAddresses": "0x533367b864D9b9AA59D0DCB6554DF0C89feEF1fF",
+  "tokenAddresses": "0x533367b864D9b9AA59D0DCB6554DF0C89feEF1fF",
+  "symbol": "USDK",
+  "isLp": false
+}, {
+  "pid": 21,
+  "lpAddresses": "0x72F8fa5da80dc6E20E00d02724cf05ebD302C35F",
+  "tokenAddresses": "0x72F8fa5da80dc6E20E00d02724cf05ebD302C35F",
+  "symbol": "DOTK",
+  "isLp": false
+}, {
+  "pid": 22,
+  "lpAddresses": "0xd616388f6533B6f1c31968a305FbEE1727F55850",
+  "tokenAddresses": "0xd616388f6533B6f1c31968a305FbEE1727F55850",
+  "symbol": "LTCK",
+  "isLp": false
+}]
 
 //address
-const oracleAddress = '0x2619a22B1e399c473cC9A3C02FcEC826679F8D00'
+const oracleAddress = '0x26061dDaFC004E8510440E696e44C38Cc3F331ec'
 const usdtAddress = '0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281'
-const chefAddress = '0x1D256cFE65cBd73dF6a9bE499a4b3486a31D807E'
-const mdxAddress = '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E'
-const mingingPoolAddress = "0xe5B876BDbfAf8e4E317cEE76889b03eb60a05E99"
+const chefAddress = '0x04964cBB5E9410721dCb6bc0503C0B4E1D19ad48'
+const mdxAddress = '0x2698598b98175bB5d20f69063d5CD99514D63819'
+const mingingPoolAddress = "0x8413fc93D1a812A26fb38FD943D10fFA478F0254"
 const USDT_DECIMAL = 10
 
 const tokens = {
@@ -200,7 +162,7 @@ const tokens = {
   '0xf6a0Dc1fD1d2c0122ab075d7ef93aD79F02CcB93': 'FILK',
   '0xd616388f6533B6f1c31968a305FbEE1727F55850': 'LTCK',
   '0x70c1c53E991F31981d592C2d865383AC0d212225': 'WOKT',
-  "0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E": "XT",
+  "0x2698598b98175bB5d20f69063d5CD99514D63819": "XT",
   // '0xae3a768f9aB104c69A7CD6041fE16fFa235d1810': 'HFIL',
   // '0x202b4936fE1a82A4965220860aE46d7d3939Bb25': 'AAVE',
   // '0x777850281719d5a96C29812ab72f822E0e09F3Da': 'SNX',
@@ -309,13 +271,13 @@ const getCoinsInfo = async() => {
   db.set('lps', []).write()
   for(const coin of coins) {
     let res = null
-    if(coin.islp) {
+    if(coin.isLp) {
       res = await calculateLp(coin.lpAddresses, coin.tokenAddresses, coin.symbol, coin.pid)
     } else {
       res = await calculateCoin(coin.lpAddresses, coin.symbol, coin.pid)
     }
     const apy = mdxPrice/Math.pow(10, USDT_DECIMAL)*SUSHI_PER_BLOCK*BLOCKS_PER_YEAR*res.poolWeight/res.totalUsdtValue*100
-    if(coin.islp) {
+    if(coin.isLp) {
       db.get('lps').push({...res, symbol: coin.symbol, apy, lpAddresses:coin.lpAddresses, pid:coin.pid}).write()
     } else {
       db.get('single').push({...res, symbol: coin.symbol, apy, lpAddresses:coin.lpAddresses, pid:coin.pid}).write()
---
+++ gencoin.js
@@ -0,0 +1,53 @@
+// const hecoAddress = 'https://exchaintest.okexcn.com'
+const hecoAddress ='http://52.73.128.127:3100/'
+const chefAddress = '0x04964cBB5E9410721dCb6bc0503C0B4E1D19ad48'
+const web3 = require('web3')
+const provider = new web3(hecoAddress)
+const erc20Abi = require('./abi/erc20.json')
+const chefAbi = require('./abi/masterchef.json')
+const pairAbi = require('./abi/pair.json')
+
+const chefContract = new provider.eth.Contract(chefAbi, chefAddress)
+
+//   {
+    // pid: 0,
+    // lpAddresses: '0x172a378B1aB64333bE2faa54C96476E5389F30AB', // 质押币地址 lpAddresses 统一用来查询余额，lp的话填写lp地址，单币的填币种地址
+    // tokenAddresses: '0xE5e399B4D0b721bD0B616E076e07E4416B78AA3E', // tokenAddresses,单币的话不需要，lp填要获取价格的币种地址
+    // symbol: 'XT/USDT',
+    // islp: true
+//   }
+
+const gencoininfo = async () => {
+    const poolLength = await chefContract.methods.poolLength().call()
+    let coins = []
+    let pid =0
+    for(; pid < poolLength; pid ++) {
+        console.log(pid)
+        const {lpToken} = await chefContract.methods.poolInfo(pid).call()
+        const isLp = pid < 13
+        let symbol = ''
+        let tokenAddresses = lpToken
+        if(isLp) {
+            const pairContract = new provider.eth.Contract(pairAbi, lpToken)
+            const token0 = await pairContract.methods.token0().call()
+            const token0Contr =  new provider.eth.Contract(erc20Abi, token0)
+            const tk0symbol = await token0Contr.methods.symbol().call()
+            const token1 = await pairContract.methods.token1().call()
+            const token1Contr =  new provider.eth.Contract(erc20Abi, token1)
+            const tk1symbol = await token1Contr.methods.symbol().call()
+            symbol = tk0symbol + "/" + tk1symbol
+            if (tk0symbol == 'USDT' || tk0symbol == 'USDC') {
+                tokenAddresses = token1
+            } else {
+                tokenAddresses = token0
+            }
+        } else {
+            const lpTokenContr =  new provider.eth.Contract(erc20Abi, lpToken)
+            symbol = await lpTokenContr.methods.symbol().call()
+        }
+        coins.push({pid, lpAddresses:lpToken, tokenAddresses, symbol, isLp})
+    }
+    console.log(JSON.stringify(coins))
+}
+
+gencoininfo()
