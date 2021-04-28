b56677b7f6116544392c6d9b93af8c414edd836a
wenjing <wenjing@gomefinance.com.cn>
2021-04-28 18:17:12 +0800

    add main option

--- start-apy.sh
+++ start-apy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-PID=$(ps -ef|grep apys.js|grep -v grep|awk '{printf $2}')
+PID=$(ps -ef|grep 'apys.js main'|grep -v grep|awk '{printf $2}')
 if [ -z "$PID" ]
 then
     cd /home/ubuntu/xswap-main/apyrobot
-    nodejs apys.js
+    nodejs apys.js main
     cp apy.json /home/ubuntu/www/main/pool.json
     echo apy end
     sleep 1
-    PID=$(ps -ef|grep apys.js|grep -v grep|awk '{printf $2}')
+  
+    PID=$(ps -ef|grep 'apys.js main'|grep -v grep|awk '{printf $2}')
     if [ -z "$PID" ]
     then
         kill -9 $PID
--- start-trans.sh
+++ start-trans.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-PID=$(ps -ef|grep transSum.js|grep -v grep|awk '{printf $2}')
+PID=$(ps -ef|grep 'transSum.js main'|grep -v grep|awk '{printf $2}')
 if [ -z "$PID" ]
 then
     cd /home/ubuntu/xswap-main/apyrobot
-    nodejs transSum.js
+    nodejs transSum.js main
     cp trans.json /home/ubuntu/www/main/trans.json
     echo transSum end
     sleep 1
-    PID=$(ps -ef|grep transSum.js|grep -v grep|awk '{printf $2}')
+    PID=$(ps -ef|grep 'transSum.js main'|grep -v grep|awk '{printf $2}')
     if [ -z "$PID" ]
     then
         kill -9 $PID
